b92bb0f9ad427a4e396b21187c9118d6782e5b2d
asankul <asankul1993@gmail.com>
2022-06-26 20:39:13 +0600

    Initial Commit

---
+++ viewWithRGB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  viewWithRGB
+//
+//  Created by Асанкул Садыков on 26/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
